798d1aaf42ce11bad5ff1ec9010b7f02ace7ffd0
Jakub Dębski <36741649+Debskij@users.noreply.github.com>
2021-03-04 22:05:23 +0100

    Fix h-gate, add tests for it

--- quantum_compiler/gates.py
+++ quantum_compiler/gates.py
@@ -1,5 +1,6 @@
-from numpy import array
 from .matrix import Matrix
+
+SQRT2 = 1 / 2 ** (1 / 2)
 
 
 class QuantumGates:
@@ -9,4 +10,4 @@ class QuantumGates:
 
     z_pauli = Matrix([[1, 0], [0, -1]])
 
-    # hadamard = Matrix(list(1 / 2 ** (1 / 2) * array([1, 1], [1, -1])))
+    hadamard = Matrix([[SQRT2, SQRT2], [SQRT2, -SQRT2]])
